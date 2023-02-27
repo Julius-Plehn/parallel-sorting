45db7a1cbb4716751f69b78f6ed9dc5cf4423830
Julius Plehn <juplehn@me.com>
2023-02-27 10:31:47 -0500

    format

--- median-of-medians/benchmark.py
+++ median-of-medians/benchmark.py
@@ -5,13 +5,13 @@ import timeit
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    #r = int(sys.argv[1])
-    #size = int(sys.argv[2])
+    # r = int(sys.argv[1])
+    # size = int(sys.argv[2])
     random.seed(10)
     rs = [5, 7, 9, 11]
     sizes = [100, 1000, 10000, 100000]
-    #sizes = [100, 1000, 10000]
-    sys.setrecursionlimit(sizes[len(sizes)-1])
+    # sizes = [100, 1000, 10000]
+    sys.setrecursionlimit(sizes[len(sizes) - 1])
 
     repeat = 1
 
@@ -21,14 +21,17 @@ if __name__ == "__main__":
     for r in rs:
         timings_median[r] = []
 
-
     for size in sizes:
-        input = list(range(1,size))
+        input = list(range(1, size))
         random.shuffle(input)
 
-        timing_sorted = timeit.timeit(stmt="sorting.insertion_sort(input.copy())", globals=globals(), number=repeat)
-        timings_sorted.append(timing_sorted/repeat)
-        print("Time: {}".format(timing_sorted/repeat))
+        timing_sorted = timeit.timeit(
+            stmt="sorting.insertion_sort(input.copy())",
+            globals=globals(),
+            number=repeat,
+        )
+        timings_sorted.append(timing_sorted / repeat)
+        print("Time: {}".format(timing_sorted / repeat))
 
         # Quicksort using last element as pivot
         """
@@ -38,10 +41,14 @@ if __name__ == "__main__":
         """
         for r in rs:
             print("Size: {}, r={}".format(size, r))
-            #sorted_input = sorting.quicksort_median(input, 0, len(input)-1, r=r)
-            timing_median = timeit.timeit(stmt="sorting.quicksort_median(input.copy(), 0, len(input)-1, r=r)", globals=globals(), number=repeat)
-            timings_median[r].append(timing_median/repeat)
-            print("Time: {}".format(timing_median/repeat))
+            # sorted_input = sorting.quicksort_median(input, 0, len(input)-1, r=r)
+            timing_median = timeit.timeit(
+                stmt="sorting.quicksort_median(input.copy(), 0, len(input)-1, r=r)",
+                globals=globals(),
+                number=repeat,
+            )
+            timings_median[r].append(timing_median / repeat)
+            print("Time: {}".format(timing_median / repeat))
 
     print(timings_quicksort)
     print(timings_sorted)
@@ -51,7 +58,7 @@ if __name__ == "__main__":
     plt.xlabel("Size")
     plt.ylabel("Time in Sec.")
     fig.set_size_inches(14, 5)
-    
+
     ax.set_xticks(sizes)
     ax.plot(
         sizes,
@@ -86,7 +93,3 @@ if __name__ == "__main__":
         ax.legend()
 
     plt.savefig("benchmark.png", dpi=200)
-
-
-    
-   
--- median-of-medians/sorting.py
+++ median-of-medians/sorting.py
@@ -2,11 +2,13 @@ import sys
 import math
 import random
 
+
 def compare(A, B):
     if sorted(A) == B:
         print("Correctly sorted")
     else:
         print("Error")
+
 
 # Version with last element as pivot element
 def partition(array, low, high):
@@ -20,36 +22,38 @@ def partition(array, low, high):
         element = array[idx]
         if element > pivot_element:
             move = array.pop(array.index(element))
-            array.insert(high,move)
+            array.insert(high, move)
             moved_high += 1
             pivot_index -= 1
         else:
-            idx +=1
+            idx += 1
 
     return (array, pivot_index)
+
 
 def quicksort(array, low, high):
     if low < high:
         array, partitioning_index = partition(array, low, high)
-        quicksort(array, low, partitioning_index-1)
-        quicksort(array, partitioning_index+1, high)
+        quicksort(array, low, partitioning_index - 1)
+        quicksort(array, partitioning_index + 1, high)
     return array
 
-def median_of_medians(array, low, high, r=5):
-    median_idx = math.ceil(r/2)-1
-    medians = []
-    block_range = high-low
-    blocks = math.ceil(block_range/r)
-    for b in range(blocks):
-        start_idx = low + b*r
-        end_idx = start_idx+r-1
-        if b==blocks-1:
-            end_idx = high
-            median_idx = math.ceil((end_idx-start_idx)/2)-1
-        medians.append(sorted(array[start_idx:end_idx+1])[median_idx])
 
-    pivot_element = sorted(medians)[math.ceil(len(medians)/2)-1]
-    pivot_index = low+array[low:high+1].index(pivot_element)
+def median_of_medians(array, low, high, r=5):
+    median_idx = math.ceil(r / 2) - 1
+    medians = []
+    block_range = high - low
+    blocks = math.ceil(block_range / r)
+    for b in range(blocks):
+        start_idx = low + b * r
+        end_idx = start_idx + r - 1
+        if b == blocks - 1:
+            end_idx = high
+            median_idx = math.ceil((end_idx - start_idx) / 2) - 1
+        medians.append(sorted(array[start_idx : end_idx + 1])[median_idx])
+
+    pivot_element = sorted(medians)[math.ceil(len(medians) / 2) - 1]
+    pivot_index = low + array[low : high + 1].index(pivot_element)
 
     array[pivot_index] = array[high]
     array[high] = pivot_element
@@ -63,20 +67,22 @@ def median_of_medians(array, low, high, r=5):
         element = array[idx]
         if element > pivot_element:
             move = array.pop(array.index(element))
-            array.insert(high,move)
+            array.insert(high, move)
             moved_high += 1
             pivot_index -= 1
         else:
-            idx +=1
+            idx += 1
 
     return (array, pivot_index)
+
 
 def quicksort_median(array, low, high, r=5):
     if low < high:
         array, partitioning_index = median_of_medians(array, low, high, r=r)
-        quicksort_median(array, low, partitioning_index-1, r=r)
-        quicksort_median(array, partitioning_index+1, high, r=r)
+        quicksort_median(array, low, partitioning_index - 1, r=r)
+        quicksort_median(array, partitioning_index + 1, high, r=r)
     return array
+
 
 def insertion_sort(array):
     for key in range(1, len(array)):
@@ -86,7 +92,8 @@ def insertion_sort(array):
             array[cmp + 1] = array[cmp]
             cmp -= 1
         array[cmp + 1] = item
-    return array        
+    return array
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -97,26 +104,26 @@ if __name__ == "__main__":
         r = int(sys.argv[2])
     f = open(sys.argv[1], "r")
     input = list(map(int, f.read().split()))
-    #print("Input:")
+    # print("Input:")
     print(input)
-    sorted_input = quicksort_median(input, 0, len(input)-1, r=r)
-    
+    sorted_input = quicksort_median(input, 0, len(input) - 1, r=r)
+
     # Check:
     compare(input, sorted_input)
 
-    input = list(range(1,1000))
+    input = list(range(1, 1000))
     random.shuffle(input)
-   
-    sorted_input = quicksort_median(input, 0, len(input)-1, r=r)
-    
+
+    sorted_input = quicksort_median(input, 0, len(input) - 1, r=r)
+
     compare(input, sorted_input)
 
-    input = list(range(1,100))
+    input = list(range(1, 100))
     random.shuffle(input)
-    sorted_input = quicksort(input, 0, len(input)-1)
+    sorted_input = quicksort(input, 0, len(input) - 1)
     compare(input, sorted_input)
 
-    test = [9,5,1,4,3]
+    test = [9, 5, 1, 4, 3]
     sorted_test = insertion_sort(test)
 
     compare(test, sorted_test)
